51ba12a5f23095bd737a6746cd5cd73cf6786bbb
Artem Didenko <artemdidenko@advoncommerce.com>
2023-01-19 17:44:50 +0200

    created modal

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,15 +2,63 @@ import React from "react";
 // import { getCurrencyPrice } from "../libs/helper";
 // import { Currencies } from "../libs/types";
 import { useStyles } from "./styles";
-
+import { Avatar, Button, Fade, Paper, Typography } from "@mui/material";
+import { useState } from "react";
+import { deepOrange } from "@mui/material/colors";
+import Popper, { PopperPlacementType } from "@mui/material/Popper";
+import BasicModal from "../modules/modals/modal.auth";
 // interface Props {
 //   currencies: Currencies;
 // }
 
 const Header = () => {
   const { classes, cx } = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
+  const [open, setOpen] = React.useState(false);
+  const [placement, setPlacement] = React.useState<PopperPlacementType>();
+  const [openAuth, setOpenAuth] = React.useState(false);
+  const handleOpen = () => setOpenAuth(true);
+  const handleClose = () => setOpenAuth(false);
+  const [name, setName] = useState("Artem");
 
-  return <div className={cx(classes.header)}>aaaaa</div>;
+  const handleClick =
+    (newPlacement: PopperPlacementType) =>
+    (event: React.MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(event.currentTarget);
+      setOpen((prev) => placement !== newPlacement || !prev);
+      setPlacement(newPlacement);
+      console.log(open);
+    };
+
+  return (
+    <div className={cx(classes.header)}>
+      <Popper open={open} anchorEl={anchorEl} placement={placement} transition>
+        {({ TransitionProps }) => (
+          <Fade {...TransitionProps} timeout={5}>
+            <Paper>
+              <Typography sx={{ p: 2 }}>
+                The content of the Popper.
+                <Button onClick={handleOpen}>Open modal</Button>
+              </Typography>
+            </Paper>
+          </Fade>
+        )}
+      </Popper>
+      <Button
+        className={cx(classes.button)}
+        onClick={handleClick("bottom-end")}
+      >
+        <Avatar sx={{ bgcolor: deepOrange[500] }} alt={name} src="#" />
+      </Button>
+      <BasicModal
+        openAuth={openAuth}
+        setOpenAuth={setOpenAuth}
+        setOpen={setOpen}
+      />
+    </div>
+  );
 };
 
 export default Header;
--- src/layout/styles.ts
+++ src/layout/styles.ts
@@ -5,18 +5,23 @@ import { Theme } from "@mui/material";
 export const useStyles = makeStyles()((theme: Theme) => ({
   header: {
     display: "flex",
-    padding: theme.spacing(2),
+    padding: theme.spacing(1),
     fontSize: 27,
     fontWeight: 700,
     alignItems: "center",
-    justifyContent: "space-around",
+    justifyContent: "flex-end",
     background: "#00000060",
     color: "white",
-    height: 60,
+    height: 40,
     [theme.breakpoints.down(600)]: {
       fontSize: 18,
       height: 40,
     },
+  },
+  button: {
+    border: "none",
+    padding: 0,
+    borderRadius: "50%",
   },
 }));
 
